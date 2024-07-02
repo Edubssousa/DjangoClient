e89ed73884512d4f546bea82ed3f2c44ca1b260d
Alisson Filipe <alissonsantos4@estudante.ifto.edu.br>
2024-07-02 15:56:16 -0300

    concertando bug

--- Client/models.py
+++ Client/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Clients(models.Model):
-    name = models.models.CharField(max_length=100)
-    email = models.models.EmailField(unique=True, max_length=254)
-    phone = models.models.CharField(max_length=20,blank=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True, max_length=254)
+    phone = models.CharField(max_length=20,blank=True)
 
     def __str__(self):
         return self.nome
