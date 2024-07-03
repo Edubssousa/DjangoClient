ba6c44d365467c4d776f6d8f2d2e55dc94c04b5c
Alisson Filipe <alifdas10@gmail.com>
2024-07-03 10:52:27 -0300

    urls e views

--- Client/urls.py
+++ Client/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path 
+from django.urls import path
 
-from . import views 
+from . import views
 
 urlpatterns = [
-    path("",views.index,name='index')
+    path("", views.addClient, name='index'),
+    path("/listClients", views.listClients, name="listClients"),
+    path("/detailsClient", views.detailsClient, name="detailsClient"),
+    path("/updateClient", views.updateClient, name="updateClient"),
+    path("/deleteClient", views.deleteClient, name="deleteClient"),
 ]
-
--- Client/views.py
+++ Client/views.py
@@ -3,5 +3,22 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("primeira view pae")
+
+def addClient(request):
+    return HttpResponse("adicionar cliente")
+
+
+def listClients(request):
+    return HttpResponse("listar clientes")
+
+
+def detailsClient(request):
+    return HttpResponse("lista de clientes")
+
+
+def updateClient(request):
+    return HttpResponse("Atualizar cliente")
+
+
+def deleteClient(request):
+    return HttpResponse("deletar cliente")
