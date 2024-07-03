a618f724093b77d3c039f15a312d53040e6a9a1d
Alisson Filipe <alissonsantos4@estudante.ifto.edu.br>
2024-07-03 17:39:38 -0300

    missing the POST

--- Client/views.py
+++ Client/views.py
@@ -24,14 +24,42 @@ def clientsList(request):
 # classe resposável pelo CRUD de um client
 @api_view(['GET', 'POST', 'PUT','DELETE'])
 def ClientCRUD(request,pk):
+
+    # VALIDAÇÃO
+
     try:
         client = Clients.objects.get(pk=pk)
     except Clients.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
+    #GET
+
     if request.method == 'GET':
         serializer = ClientSerializer(client)
         return Response(serializer.data)
+
+    
+    #POST
+
+#    if request.method == 'POST':
+#       new_client = request.data
+#        serializer = ClientSerializer(new_client)
+#        if serializer.is_valid():
+#            serializer.save()
+#            return Response(serializer.data, status=status.HTTP_201_CREATED)
+#        return Response(status.HTTP_400_BAD_REQUEST)  
+
+
+    #PUT
+
+    if request.method == 'PUT':
+        serializer = ClientSerializer(client,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    #DELETE
 
     elif request.method == 'DELETE':
         client.delete()
