61b095bba0ac05e2e1ced7dd567695c50dfe330a
Alisson Filipe <alifdas10@gmail.com>
2024-07-03 09:17:47 -0300

    concertando return name

--- Client/models.py
+++ Client/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+
 class Clients(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True, max_length=254)
-    phone = models.CharField(max_length=20,blank=True)
+    phone = models.CharField(max_length=20, blank=True)
 
     def __str__(self):
-        return self.nome
+        return self.name
