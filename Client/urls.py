12d7e733610c93db1e47a382d43baf10eda89190
Alisson Filipe <alifdas10@gmail.com>
2024-07-04 10:01:17 -0300

    só gratidão

--- Client/urls.py
+++ Client/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-
 from . import views
+
 
 urlpatterns = [
     path("", views.clientsList, name="clientsList"),
-    path("ClientCrud/<int:pk>/", views.ClientCRUD, name='index'),# testar o pk que talvez seja id
-   
+    path("ClientCrud/<int:pk>/", views.ClientCRUD,
+         name='index'),  # testar o pk que talvez seja id
+    path("createClient/", views.createClient, name='createClient')
 ]
--- Client/views.py
+++ Client/views.py
@@ -1,3 +1,4 @@
+from rest_framework import generics  # necessário para o create
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import render
@@ -11,19 +12,18 @@ from .serializers import ClientSerializer
 # pra falar a verdade precimos de apenas duas views
 
 
-
 @api_view(['GET'])
 def clientsList(request):
     if request.method == 'GET':
         clients = Clients.objects.all()
-        serializer = ClientSerializer(clients,many=True)
+        serializer = ClientSerializer(clients, many=True)
         return Response(serializer.data)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
 
-# classe resposável pelo CRUD de um client
-@api_view(['GET', 'POST', 'PUT','DELETE'])
-def ClientCRUD(request,pk):
+
+# classe resposável pelo RUD de um client exeto CREATE
+@api_view(['GET', 'PUT', 'DELETE'])
+def ClientCRUD(request, pk):
 
     # VALIDAÇÃO
 
@@ -32,38 +32,34 @@ def ClientCRUD(request,pk):
     except Clients.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
-    #GET
+    # GET
 
     if request.method == 'GET':
         serializer = ClientSerializer(client)
         return Response(serializer.data)
 
-    
-    #POST
-
-#    if request.method == 'POST':
-#       new_client = request.data
-#        serializer = ClientSerializer(new_client)
-#        if serializer.is_valid():
-#            serializer.save()
-#            return Response(serializer.data, status=status.HTTP_201_CREATED)
-#        return Response(status.HTTP_400_BAD_REQUEST)  
-
-
-    #PUT
+    # PUT
 
     if request.method == 'PUT':
-        serializer = ClientSerializer(client,data=request.data)
+        serializer = ClientSerializer(client, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    #DELETE
+    # DELETE
 
     elif request.method == 'DELETE':
         client.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-
+# CREATE
+@api_view(['POST'])
+def createClient(request):
+    if request.method == 'POST':
+        serializer = ClientSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
